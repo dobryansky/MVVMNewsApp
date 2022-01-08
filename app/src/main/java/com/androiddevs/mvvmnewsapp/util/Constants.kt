87b5527f52ae268689a1ee86371f9a1d95a3e258
Artem <mikstrip@yandex.ru>
2022-01-08 21:17:25 +0300

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "e4974e0cfb3d4b58bb220b8d4b246699"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
